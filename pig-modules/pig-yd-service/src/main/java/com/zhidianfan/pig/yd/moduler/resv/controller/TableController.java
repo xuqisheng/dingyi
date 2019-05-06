4112033df66fc83528f324ed3464f7c60345d93c
王烨州 <>
2019-05-06 14:59:18 +0800

    新增热点接口

--- pig-modules/pig-yd-service/src/main/java/com/zhidianfan/pig/yd/moduler/resv/controller/TableController.java
+++ pig-modules/pig-yd-service/src/main/java/com/zhidianfan/pig/yd/moduler/resv/controller/TableController.java
@@ -1,14 +1,19 @@
 package com.zhidianfan.pig.yd.moduler.resv.controller;
 
 
+import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import com.zhidianfan.pig.yd.moduler.common.dao.entity.TableArea;
+import com.zhidianfan.pig.yd.moduler.common.dao.entity.TableAreaImageDO;
 import com.zhidianfan.pig.yd.moduler.common.dao.entity.TableType;
 import com.zhidianfan.pig.yd.moduler.common.dto.ErrorTip;
 import com.zhidianfan.pig.yd.moduler.common.dto.SuccessTip;
 import com.zhidianfan.pig.yd.moduler.common.dto.Tip;
 import com.zhidianfan.pig.yd.moduler.common.service.ITableAreaService;
+import com.zhidianfan.pig.yd.moduler.common.service.ITableService;
 import com.zhidianfan.pig.yd.moduler.common.service.ITableTypeService;
 import com.zhidianfan.pig.yd.moduler.resv.bo.DeskBo;
 import com.zhidianfan.pig.yd.moduler.resv.dto.BusinessTableTypeDTO;
@@ -17,9 +22,14 @@ import com.zhidianfan.pig.yd.moduler.resv.dto.TableAreaDTO;
 import com.zhidianfan.pig.yd.moduler.resv.dto.TablesDTO;
 import com.zhidianfan.pig.yd.moduler.resv.service.BusinessTableTypeService;
 import com.zhidianfan.pig.yd.moduler.resv.service.TableService;
+import com.zhidianfan.pig.yd.moduler.resv.vo.TableHotMapVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.apache.commons.collections.CollectionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cglib.core.Local;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,6 +38,7 @@ import javax.validation.Valid;
 import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @Description:桌位
@@ -39,6 +50,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/table")
 public class TableController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TableController.class);
 
     @Resource
     private TableService tableService;
@@ -52,18 +65,20 @@ public class TableController {
     @Resource
     private ITableTypeService iTableTypeService;
 
-
+    @Resource
+    private ITableService iTableService;
 
 
     /**
      * 所有桌位
+     *
      * @param businessId 酒店id
      * @param status
      * @return
      */
     @ApiOperation("所有桌位")
-    @GetMapping(value = "/list",params = {"businessId"})
-    public ResponseEntity<List<DeskBo>> list(Integer businessId, String status){
+    @GetMapping(value = "/list", params = {"businessId"})
+    public ResponseEntity<List<DeskBo>> list(Integer businessId, String status) {
 
         List<DeskBo> tables = tableService.deskList(businessId, status);
         return ResponseEntity.ok(tables);
@@ -71,7 +86,7 @@ public class TableController {
 
     @ApiOperation("添加")
     @PostMapping(value = "/add")
-    public ResponseEntity<Tip> add(@Valid TablesDTO tablesDTO){
+    public ResponseEntity<Tip> add(@Valid TablesDTO tablesDTO) {
 
         Tip tip = tableService.tableAdd(tablesDTO);
         return ResponseEntity.ok(tip);
@@ -80,7 +95,7 @@ public class TableController {
 
     @ApiOperation("编辑（包括删除）")
     @PostMapping(value = "/edit")
-    public ResponseEntity<Object> edit(@Valid @RequestBody TablesDTO tablesDTO){
+    public ResponseEntity<Object> edit(@Valid @RequestBody TablesDTO tablesDTO) {
 
         Object tip = tableService.tableEdit(tablesDTO);
         return ResponseEntity.ok(tip);
@@ -88,23 +103,23 @@ public class TableController {
 
     @ApiOperation("新增桌型")
     @PostMapping(value = "/type/add")
-    public  ResponseEntity<Tip> typeAdd(@Valid BusinessTableTypeDTO businessTableTypeDTO){
+    public ResponseEntity<Tip> typeAdd(@Valid BusinessTableTypeDTO businessTableTypeDTO) {
 
         boolean insert = businessTableTypeService.typeAdd(businessTableTypeDTO);
-        return ResponseEntity.ok(insert? SuccessTip.SUCCESS_TIP: ErrorTip.ERROR_TIP);
+        return ResponseEntity.ok(insert ? SuccessTip.SUCCESS_TIP : ErrorTip.ERROR_TIP);
     }
 
     @ApiOperation("编辑桌型（包括删除）")
     @PostMapping(value = "/type/edit")
-    public  ResponseEntity<Tip> typeEdit(@Valid  BusinessTableTypeDTO businessTableTypeDTO){
+    public ResponseEntity<Tip> typeEdit(@Valid BusinessTableTypeDTO businessTableTypeDTO) {
 
         boolean insert = businessTableTypeService.typeEdit(businessTableTypeDTO);
-        return ResponseEntity.ok(insert? SuccessTip.SUCCESS_TIP:ErrorTip.ERROR_TIP);
+        return ResponseEntity.ok(insert ? SuccessTip.SUCCESS_TIP : ErrorTip.ERROR_TIP);
     }
 
     @ApiOperation("所有桌型")
     @GetMapping(value = "/type/all")
-    public  ResponseEntity<List<TableType>> typeAdd(Integer businessId){
+    public ResponseEntity<List<TableType>> typeAdd(Integer businessId) {
         TableType condition = new TableType();
         condition.setBusinessId(businessId);
         List<TableType> tableTypes = iTableTypeService.selectList(new EntityWrapper<>(condition));
@@ -112,14 +127,13 @@ public class TableController {
     }
 
 
-
     @ApiOperation("所有区域")
     @GetMapping(value = "/area/all")
-    public  ResponseEntity<List<TableArea>> areaAdd(Integer businessId, Integer status){
+    public ResponseEntity<List<TableArea>> areaAdd(Integer businessId, Integer status) {
         TableArea condition = new TableArea();
         condition.setBusinessId(businessId);
-        if(status!=null){
-            condition.setStatus(status+"");
+        if (status != null) {
+            condition.setStatus(status + "");
         }
         List<TableArea> tableTypes = iTableAreaService.selectList(new EntityWrapper<>(condition));
         return ResponseEntity.ok(tableTypes);
@@ -127,19 +141,19 @@ public class TableController {
 
     @ApiOperation("新增区域")
     @PostMapping(value = "/area/add")
-    public  ResponseEntity<Tip> areaAdd(@Valid TableAreaDTO tableAreaDTO){
+    public ResponseEntity<Tip> areaAdd(@Valid TableAreaDTO tableAreaDTO) {
 
         boolean insert = tableService.areaAdd(tableAreaDTO);
-        return ResponseEntity.ok(insert? SuccessTip.SUCCESS_TIP:ErrorTip.ERROR_TIP);
+        return ResponseEntity.ok(insert ? SuccessTip.SUCCESS_TIP : ErrorTip.ERROR_TIP);
     }
 
 
     @ApiOperation("编辑区域（包括删除）")
     @PostMapping(value = "/area/edit")
-    public  ResponseEntity<Tip> areaEdit(@Valid @RequestBody TableAreaDTO tableAreaDTO){
+    public ResponseEntity<Tip> areaEdit(@Valid @RequestBody TableAreaDTO tableAreaDTO) {
 
         boolean b = tableService.areaEdit(tableAreaDTO);
-        return ResponseEntity.ok(b? SuccessTip.SUCCESS_TIP:ErrorTip.ERROR_TIP);
+        return ResponseEntity.ok(b ? SuccessTip.SUCCESS_TIP : ErrorTip.ERROR_TIP);
     }
 
     /**
@@ -147,11 +161,45 @@ public class TableController {
      */
     @ApiOperation("查询一个酒店该天该餐别的空闲桌位的最大容纳人数")
     @GetMapping(value = "/capacity")
-    public  ResponseEntity<Integer> getFreeTableMaxCapacity(FreeTableCapacityDTO freeTableCapacityDTO){
+    public ResponseEntity<Integer> getFreeTableMaxCapacity(FreeTableCapacityDTO freeTableCapacityDTO) {
 
-        Integer num = tableService.getFreeTableMaxCapacity(freeTableCapacityDTO.getBusinessid(),freeTableCapacityDTO.getMealtypeid(),freeTableCapacityDTO.getResvdate());
+        Integer num = tableService.getFreeTableMaxCapacity(freeTableCapacityDTO.getBusinessid(), freeTableCapacityDTO.getMealtypeid(), freeTableCapacityDTO.getResvdate());
         return ResponseEntity.ok(num);
     }
 
 
+    @GetMapping("/hotMap")
+    public ResponseEntity<Map<String, Object>> getTableHotMap(Integer businessId, Integer tableAreaId) {
+        if (businessId == null || tableAreaId == null) {
+            LOGGER.info("getTableHotMap:请求参数为空 businessId:" + businessId + "     tableAreaId:" + tableAreaId);
+            return ResponseEntity.ok().build();
+        }
+        Integer hotMapSwitch = iTableService.businessHotMapSwitch(businessId);
+        if (Integer.valueOf(0).equals(hotMapSwitch)) {
+            LOGGER.info("getTableHotMap:酒店没有开启配置 businessId:" + businessId);
+            return ResponseEntity.ok().build();
+        }
+
+        List<TableAreaImageDO> tableAreaImages = iTableService.queryTableAreaImage(businessId);
+        if (CollectionUtils.isEmpty(tableAreaImages)) {
+            LOGGER.info("酒店没有配置该区域地图   businessId:" + businessId + "     tableAreaId:" + tableAreaId);
+            return ResponseEntity.ok().build();
+        }
+        List<TableHotMapVO> resultVO = Lists.newArrayList();
+        for (TableAreaImageDO tableAreaImage : tableAreaImages) {
+            TableHotMapVO tableHotMap = new TableHotMapVO();
+            tableHotMap.setImageUrl(tableAreaImage.getImageUrl());
+            tableHotMap.setBusinessId(tableAreaImage.getBusinessId());
+            tableHotMap.setTableAreaId(tableAreaImage.getTableAreaId());
+            tableHotMap.setTableImages(iTableService.queryTableImage(tableAreaId, businessId));
+            resultVO.add(tableHotMap);
+        }
+        LOGGER.debug(businessId + "请求热点图结果:" + JSONObject.toJSONString(resultVO));
+
+        Map<String, Object> result = Maps.newHashMap();
+        result.put("data", resultVO);
+        result.put("error_code", 0);
+        result.put("error_msg", "");
+        return ResponseEntity.ok(result);
+    }
 }
