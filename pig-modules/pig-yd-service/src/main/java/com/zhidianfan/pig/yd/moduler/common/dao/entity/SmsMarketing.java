1692c27f98fa3fb652cecda29170f2686e6b14fd
胡泽鹏啊 <qasd7531@outlook.com>
2019-06-11 16:24:35 +0800

    生日短信发送表准备

--- pig-modules/pig-yd-service/src/main/java/com/zhidianfan/pig/yd/moduler/common/dao/entity/SmsMarketing.java
+++ pig-modules/pig-yd-service/src/main/java/com/zhidianfan/pig/yd/moduler/common/dao/entity/SmsMarketing.java
@@ -1,21 +1,20 @@
 package com.zhidianfan.pig.yd.moduler.common.dao.entity;
 
-import com.baomidou.mybatisplus.activerecord.Model;
-import com.baomidou.mybatisplus.annotations.TableField;
-import com.baomidou.mybatisplus.annotations.TableId;
-import com.baomidou.mybatisplus.annotations.TableName;
 import com.baomidou.mybatisplus.enums.IdType;
-
-import java.io.Serializable;
 import java.util.Date;
+import com.baomidou.mybatisplus.annotations.TableId;
+import com.baomidou.mybatisplus.annotations.TableField;
+import com.baomidou.mybatisplus.activerecord.Model;
+import com.baomidou.mybatisplus.annotations.TableName;
+import java.io.Serializable;
 
 /**
  * <p>
  * 
  * </p>
  *
- * @author Conan
- * @since 2018-09-10
+ * @author huzp
+ * @since 2019-06-11
  */
 @TableName("sms_marketing")
 public class SmsMarketing extends Model<SmsMarketing> {
@@ -25,21 +24,42 @@ public class SmsMarketing extends Model<SmsMarketing> {
     @TableId(value = "id", type = IdType.AUTO)
     private Integer id;
     private String content;
+    /**
+     * 模板变量信息
+     */
     private String variable;
     @TableField("business_id")
     private Integer businessId;
+    /**
+     * 发送类型0：即时发送,1：定时发送
+     */
     @TableField("send_type")
     private Integer sendType;
-    @TableField("`status`")
+    /**
+     * 1、商户提交审核 2、审核不通过 3、 审核通过  4、商户已发送 5、创蓝提交失败 6、创蓝审核不通过
+     */
     private String status;
+    /**
+     * 定时发送时间
+     */
     private Date timer;
     @TableField("created_at")
     private Date createdAt;
     @TableField("updated_at")
     private Date updatedAt;
     private String remark;
+    /**
+     * 自定义发送条件
+     */
     private String custom;
+    /**
+     * 发送号码
+     */
+    @TableField("target_phones")
     private String targetPhones;
+    /**
+     * 发送目标手机数量(也可能是一条短信的数量)
+     */
     private Integer num;
     /**
      * 短信总数  根据短信的每条乘以发送人数计算
@@ -59,10 +79,59 @@ public class SmsMarketing extends Model<SmsMarketing> {
     @TableField("campaign_id")
     private Integer campaignId;
     private Integer sex;
+    /**
+     * 接收的客户
+     */
+    @TableField("accept_custome")
+    private String acceptCustome;
+    /**
+     * 锁
+     */
     private Integer version;
+    /**
+     * 客户端,0历史数据,1:安卓电话机
+     */
     private Integer client;
+    /**
+     * 安卓电话机短信发送未通过审核是否返还短信
+     */
+    @TableField("is_refund")
+    private Integer isRefund;
+    /**
+     * 营销模板id 对应business_marketing_sms_template表
+     */
+    @TableField("template_id")
+    private Integer templateId;
+    /**
+     * 1 用户类型 2自定义 3 模板 
+     */
+    @TableField("sms_edit_type")
+    private Integer smsEditType;
+    /**
+     * 营销经理
+     */
+    @TableField("app_user_id")
+    private Integer appUserId;
+    /**
+     * 审核时间
+     */
+    @TableField("auditing_at")
+    private Date auditingAt;
+    /**
+     * 审核人账号，一般为总后台用户
+     */
+    @TableField("auditing_user_id")
+    private String auditingUserId;
+    /**
+     * 短信 msgid
+     */
+    private String msgid;
+    /**
+     * 错误信息
+     */
     @TableField("error_msg")
     private String errorMsg;
+
 
     public Integer getId() {
         return id;
@@ -232,6 +301,14 @@ public class SmsMarketing extends Model<SmsMarketing> {
         this.sex = sex;
     }
 
+    public String getAcceptCustome() {
+        return acceptCustome;
+    }
+
+    public void setAcceptCustome(String acceptCustome) {
+        this.acceptCustome = acceptCustome;
+    }
+
     public Integer getVersion() {
         return version;
     }
@@ -246,6 +323,62 @@ public class SmsMarketing extends Model<SmsMarketing> {
 
     public void setClient(Integer client) {
         this.client = client;
+    }
+
+    public Integer getIsRefund() {
+        return isRefund;
+    }
+
+    public void setIsRefund(Integer isRefund) {
+        this.isRefund = isRefund;
+    }
+
+    public Integer getTemplateId() {
+        return templateId;
+    }
+
+    public void setTemplateId(Integer templateId) {
+        this.templateId = templateId;
+    }
+
+    public Integer getSmsEditType() {
+        return smsEditType;
+    }
+
+    public void setSmsEditType(Integer smsEditType) {
+        this.smsEditType = smsEditType;
+    }
+
+    public Integer getAppUserId() {
+        return appUserId;
+    }
+
+    public void setAppUserId(Integer appUserId) {
+        this.appUserId = appUserId;
+    }
+
+    public Date getAuditingAt() {
+        return auditingAt;
+    }
+
+    public void setAuditingAt(Date auditingAt) {
+        this.auditingAt = auditingAt;
+    }
+
+    public String getAuditingUserId() {
+        return auditingUserId;
+    }
+
+    public void setAuditingUserId(String auditingUserId) {
+        this.auditingUserId = auditingUserId;
+    }
+
+    public String getMsgid() {
+        return msgid;
+    }
+
+    public void setMsgid(String msgid) {
+        this.msgid = msgid;
     }
 
     public String getErrorMsg() {
@@ -274,6 +407,7 @@ public class SmsMarketing extends Model<SmsMarketing> {
         ", createdAt=" + createdAt +
         ", updatedAt=" + updatedAt +
         ", remark=" + remark +
+        ", custom=" + custom +
         ", targetPhones=" + targetPhones +
         ", num=" + num +
         ", smsNum=" + smsNum +
@@ -284,7 +418,16 @@ public class SmsMarketing extends Model<SmsMarketing> {
         ", userName=" + userName +
         ", campaignId=" + campaignId +
         ", sex=" + sex +
+        ", acceptCustome=" + acceptCustome +
         ", version=" + version +
+        ", client=" + client +
+        ", isRefund=" + isRefund +
+        ", templateId=" + templateId +
+        ", smsEditType=" + smsEditType +
+        ", appUserId=" + appUserId +
+        ", auditingAt=" + auditingAt +
+        ", auditingUserId=" + auditingUserId +
+        ", msgid=" + msgid +
         ", errorMsg=" + errorMsg +
         "}";
     }
