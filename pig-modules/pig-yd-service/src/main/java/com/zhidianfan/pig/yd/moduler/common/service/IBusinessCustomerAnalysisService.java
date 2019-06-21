3ec480b807975add528f7126cbbec843fd384476
施杰灵 <shijieling123@163.com>
2019-06-21 22:38:35 +0800

    客户分析-实体类

---
+++ pig-modules/pig-yd-service/src/main/java/com/zhidianfan/pig/yd/moduler/common/service/IBusinessCustomerAnalysisService.java
@@ -0,0 +1,16 @@
+package com.zhidianfan.pig.yd.moduler.common.service;
+
+import com.zhidianfan.pig.yd.moduler.common.dao.entity.BusinessCustomerAnalysis;
+import com.baomidou.mybatisplus.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author qqx
+ * @since 2019-06-21
+ */
+public interface IBusinessCustomerAnalysisService extends IService<BusinessCustomerAnalysis> {
+
+}
